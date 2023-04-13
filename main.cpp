e0289dc81a68d1c2028426fe78c17a4aae8928f4
kivva33 <kenlil@bk.ru>
2023-04-14 01:29:51 +0300

    third

--- main.cpp
+++ main.cpp
@@ -2,9 +2,6 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    for(int i = 0; i < 10; i++){
-        std::cout << i << std::endl;
-    }
     std::cout << "That's all";
     return 0;
 }
